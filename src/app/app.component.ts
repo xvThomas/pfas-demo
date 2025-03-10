7faab89c6eb35f148b31adadc16b7a8e939bd2a8
Xavier Thomas <xvthomas@gmail.com>
2025-03-10 17:41:16 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MapComponent} from './map/map.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MapComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'pfas-demo';
+
+  constructor() {
+    console.log("new app component");
+  }
 }
